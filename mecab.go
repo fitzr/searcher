d307738d8a1a19671d06e3f8bfd88a136d308b0f
fitzr <fitzroy.t.hagi@gmail.com>
2016-09-09 15:06:51 +0900

    Fix format

--- mecab.go
+++ mecab.go
@@ -1,39 +1,39 @@
 package searcher
 
-import(
-    "github.com/shogo82148/go-mecab"
-    "strings"
+import (
+	"github.com/shogo82148/go-mecab"
+	"strings"
 )
 
 type mecabParser struct {
-    model mecab.Model
-    tagger mecab.MeCab
+	model  mecab.Model
+	tagger mecab.MeCab
 }
 
 func newMeCab() *mecabParser {
-    model, err := mecab.NewModel(map[string]string{"output-format-type": "wakati"})
-    if err != nil {
-        panic(err)
-    }
+	model, err := mecab.NewModel(map[string]string{"output-format-type": "wakati"})
+	if err != nil {
+		panic(err)
+	}
 
-    tagger, err := model.NewMeCab()
-    if err != nil {
-        panic(err)
-    }
+	tagger, err := model.NewMeCab()
+	if err != nil {
+		panic(err)
+	}
 
-    return &mecabParser{model:model,tagger:tagger}
+	return &mecabParser{model: model, tagger: tagger}
 }
 
 func (m *mecabParser) destroy() {
-    m.tagger.Destroy()
-    m.model.Destroy()
+	m.tagger.Destroy()
+	m.model.Destroy()
 }
 
-func (m *mecabParser) parse(s string) []string{
-    ret, err := m.tagger.Parse(s)
-    if err != nil {
-        panic(err)
-    }
-    ret = ret[:len(ret)-2] // remove last space
-    return strings.Split(ret, " ")
+func (m *mecabParser) parse(s string) []string {
+	ret, err := m.tagger.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+	ret = ret[:len(ret)-2] // remove last space
+	return strings.Split(ret, " ")
 }
--- mecab_test.go
+++ mecab_test.go
@@ -1,18 +1,18 @@
 package searcher
 
 import (
-    "testing"
-    "reflect"
+	"reflect"
+	"testing"
 )
 
 func TestIndex(t *testing.T) {
-    input := "もう眠い"
-    expected := []string{"もう","眠い"}
+	input := "もう眠い"
+	expected := []string{"もう", "眠い"}
 
-    sut := newMeCab()
-    actual := sut.parse(input)
+	sut := newMeCab()
+	actual := sut.parse(input)
 
-    if !reflect.DeepEqual(expected, actual) {
-        t.Errorf("\nexpected: %v\nactual: %v", expected, actual)
-    }
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("\nexpected: %v\nactual: %v", expected, actual)
+	}
 }
